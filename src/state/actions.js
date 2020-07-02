8ac8b86fe6e4142552b78305b829cdf4b96cffa2
colliel <jam1986@inbox.ru>
2020-07-02 23:49:15 +0500

    23.49

--- src/state/actions.js
+++ src/state/actions.js
@@ -1,4 +1,4 @@
-import {FETCH_GOODS} from './types'
+import {FETCH_GOODS, SHOW_LOADER, HIDE_LOADER} from './types'
 
 const url = process.env.REACT_APP_BD_URL
 
@@ -6,13 +6,20 @@ const apiKey = 'AIzaSyCh3ScfjZQd23KVcyyAbmDdrBqo_bKLpEU'
 
 export const fetchGoods = () => {
     return (dispatch) => {
+        dispatch({type: SHOW_LOADER})
         return fetch(`${url}/goods.json`, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json'
             }
         }).then(response => response.json())
-            .then(data => Object.keys(data).map(item => data[item]))
-            .then(arr => dispatch({type: FETCH_GOODS, payload: arr}))
+            .then(data => {
+                console.log(data)
+                Object.keys(data).map(item => data[item])
+            })
+            .then(arr => {
+                dispatch({type: FETCH_GOODS, payload: arr})
+            })
+            .then(() => dispatch({type: HIDE_LOADER}))
     }
 }
